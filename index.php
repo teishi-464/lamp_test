daf961081f662553f305ec53b09b723b891a5228
teishi-464 <y.koizumi.sys19@morijyobi.ac.jp>
2020-06-16 12:22:02 +0900

    test

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    $dsn = 'mysql:dbname=sample_db;host=localhost;';
+    $user = 'otsubo';
+    $password = 'morijyobi';
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+        %sql = "select * from user;"
+        %result = $dbh->query(%sql);
+    } catch (PDOException $e) {
+        print "Failed: " . $e->getMessage() . "\n";
+        exit();
+    }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php foreach( $res as $value) { ?>
+
+        <h1><?php echo "$value[name]"; ?></h1>
+
+    <?php } ?>
+</body>
+</html>
