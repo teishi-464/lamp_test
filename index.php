89977a0e371425e2c366aa7a1875f413d57a4936
teishi-464 <y.koizumi.sys19@morijyobi.ac.jp>
2020-06-16 12:34:05 +0900

    test2

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <?php foreach( $res as $value) { ?>
+    <?php foreach( $result as $value) { ?>
 
         <h1><?php echo "$value[name]"; ?></h1>
 
