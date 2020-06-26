fb98b92180f8caa71d7f9e8ff341b94bc4a02ef8
teishi-464 <y.koizumi.sys19@morijyobi.ac.jp>
2020-06-26 12:43:33 +0900

    fix insert php

--- insert.php
+++ insert.php
@@ -6,9 +6,9 @@
         $dbh = new PDO($dsn, $user, $password);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $_POST['id'];
-        $_POST['name'];
-        $_POST['age'];
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $age = $_POST['age'];
 
         $sql = "insert into user values (:id, :name, :age)";
         $stmt = $dbh->prepare($sql);
@@ -18,7 +18,7 @@
         header('Location: index.php?fg=1')
 
     } catch (PDOException $e) {
-        print "Location: index.php?fg=2?err=" . $e->getMessage() . "\n";
+        header('Location: index.php?fg=2?err='.$e->getMessage());
         exit();
     }
 ?>
