3c1de51926cf2fd1fdcf35fa0f591448479f2169
teishi-464 <y.koizumi.sys19@morijyobi.ac.jp>
2020-06-29 11:34:44 +0900

    fix update2

--- update.php
+++ update.php
@@ -15,7 +15,7 @@
         $prams = array(':id'=> $id, ':name'=> $name, ':age'=> $age,);
         $stmt->execute($prams);
 
-        header('Location: index.php?fg=1')
+        header('Location: index.php?fg=1');
 
     } catch (PDOException $e) {
         header('Location: index.php?fg=2?err='.$e->getMessage());
